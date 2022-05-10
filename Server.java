232be4ac521c4e6d57f2b4a9a5d84c2d70f3d77b
alikates <alikates@dnyon.com>
2022-05-10 20:47:14 +0200

    Add generic methods for running services

--- Server.java
+++ Server.java
@@ -1,7 +1,20 @@
+import java.lang.reflect.InvocationTargetException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface Server extends Remote {
-    public static String IP_port=null;
-    public static String name = null;
+    public class Service {
+        public String name;
+        public ArrayList<Object> params;
+    }
+
+    String getName();
+
+    String getIpPort();
+
+    ArrayList<String> getServices() throws RemoteException;
+
+    Object executeService(String service, Object... params) throws RemoteException, IllegalAccessException,
+            InvocationTargetException, NoSuchMethodException, SecurityException;
 }
--- Server651Impl.java
+++ Server651Impl.java
@@ -1,20 +1,23 @@
 import java.rmi.server.UnicastRemoteObject;
+import java.lang.reflect.InvocationTargetException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.time.LocalDateTime;
 
 public class Server651Impl extends UnicastRemoteObject implements Server {
     private String IP_port; // server's IP and Port
-    private static String name; // server's name
+    private String name; // server's name
+    private ArrayList<String> services = new ArrayList<>(); // Service list
 
     protected Server651Impl(String address, String name) throws RemoteException {
         super();
         this.IP_port = address;
         this.name = name;
+        services.add("getTimeString");
     }
 
-    
     public static String getTimeString() throws RemoteException {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
@@ -39,5 +42,26 @@ public class Server651Impl extends UnicastRemoteObject implements Server {
         } catch (Exception ex) {
             System.out.println(ex);
         }
+    }
+
+    @Override
+    public ArrayList<String> getServices() throws RemoteException {
+        return this.services;
+    }
+
+    @Override
+    public Object executeService(String service, Object... params) throws RemoteException, IllegalAccessException,
+            InvocationTargetException, NoSuchMethodException, SecurityException {
+        return this.getClass().getMethod(service).invoke(this, params);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getIpPort() {
+        return this.IP_port;
     }
 }
